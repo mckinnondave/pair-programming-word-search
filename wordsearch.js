d8aeacb6013b45c44c1c572621de551e10770daa
Dave McKinnon <dave.w.mckinnon@gmail.com>
2022-03-16 23:42:37 +0000

    function that takes in an array of arrays that form a wordsearch grid, and searches for a given word

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+    let transposedArray = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      transposedArray[i] = [];
+      for (let j = 0; j < matrix.length; j++) {
+        transposedArray[i][j] = matrix[j][i];
+      }
     }
-}
-
-module.exports = wordSearch
+    return transposedArray;
+  };
+  
+  const wordSearch = (letters, word) => {
+    const verticalJoin = transpose(letters).map(ls => ls.join(""));
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+    for (l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+    return false;
+  };
+  
+  module.exports = wordSearch;
